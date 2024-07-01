20559ea723de84bfa07fc622d670ac071ba0b371
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-07-01 15:05:28 +0700

    remove: clear unusable code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CarsPage from "./pages/cars/carsPage";
 import Home from "./pages/home/home";
