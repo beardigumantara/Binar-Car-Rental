0d0790d0fa608f9e3472600706efde65545b2c03
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-07-01 16:37:43 +0700

    set:config env

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    define: {
+      APP_ENV: JSON.stringify(process.env.VITE_VERCEL_ENV),
+    },
+  };
+});
